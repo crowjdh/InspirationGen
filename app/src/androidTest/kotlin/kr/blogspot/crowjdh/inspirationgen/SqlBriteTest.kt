1a7ff34e852c47d2eb28d3dd492f6370626ca338
Chris Jeong <crowjdh@gmail.com>
2016-05-17 14:10:45 +0900

    Change name of test class

--- app/src/androidTest/kotlin/kr/blogspot/crowjdh/inspirationgen/SqlBriteTest.kt
+++ app/src/androidTest/kotlin/kr/blogspot/crowjdh/inspirationgen/SqlBriteTest.kt
@@ -16,7 +16,7 @@ import org.junit.runner.RunWith
  * AndroidJUnitSampleTest
  */
 @RunWith(AndroidJUnit4::class)
-class AndroidJUnitSampleTest():
+class SqlBriteTest():
         ApplicationTestCase<InspirationGenApplication>(InspirationGenApplication::class.java) {
 
     companion object {
